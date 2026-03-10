e8c41cc545610d3201f871d1004b57e38fa5783f
jblik <88430125+jblik@users.noreply.github.com>
2026-03-10 15:19:42 +0100

    add comment home-manager

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -27,10 +27,10 @@
     };
     
     # todo use home manager
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+#    home-manager = {
+#      url = "github:nix-community/home-manager";
+#      inputs.nixpkgs.follows = "nixpkgs";
+#    };
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, homebrew-argoproj }:
