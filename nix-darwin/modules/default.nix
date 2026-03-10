e9fc65a8e72d2dbf2fc8ab9fc459e3efb87d3886
jblik <88430125+jblik@users.noreply.github.com>
2026-03-10 09:50:16 +0100

    don't use system part

--- nix-darwin/modules/default.nix
+++ nix-darwin/modules/default.nix
@@ -1,8 +1,13 @@
 # reference other modules
 {
-  imports = [
-    ./apps/default.nix
-    ./system/default.nix
-    ./security/default.nix
-  ];
+  # imports = [
+  #   ./apps/default.nix
+  #   ./system/default.nix
+  #   ./security/default.nix
+  # ];
+    system.defaults.loginwindow = {
+    GuestEnabled = false; # Disable guest account
+    LoginwindowText = "Is this yours?"; # Set login window text
+    # Add more loginwindow settings here
+  };
 }
