6a111bef2fa8e15e53a8e78e55427d7950d6454c
jblik <88430125+jblik@users.noreply.github.com>
2026-03-10 14:43:12 +0100

    some dock settings

--- nix-darwin/modules/default.nix
+++ nix-darwin/modules/default.nix
@@ -1,13 +1,8 @@
 # reference other modules
 {
-  # imports = [
-  #   ./apps/default.nix
-  #   ./system/default.nix
-  #   ./security/default.nix
-  # ];
-    system.defaults.loginwindow = {
-    GuestEnabled = false; # Disable guest account
-    LoginwindowText = "Is this yours?"; # Set login window text
-    # Add more loginwindow settings here
-  };
+  imports = [
+    ./apps/default.nix
+    ./system/default.nix
+    ./security/default.nix
+  ];
 }
--- nix-darwin/modules/system/appearance.nix
+++ nix-darwin/modules/system/appearance.nix
@@ -9,6 +9,20 @@ in
     orientation = "bottom";
     persistent-apps = [
       "/${pkgs.iterm2}/Applications/iTerm2.app"
+      "/Applications/Safari.app"
+      # todo get these from nix as well
+      "${homeDirectory}/Rider.app"
+      "${homeDirectory}/DataGrip.app"
+      "${homeDirectory}/IntelliJ IDEA.app"
+      "${homeDirectory}/WebStorm.app"
+      "${homeDirectory}/PyCharm.app"
+      "${homeDirectory}/RustRover.app"
+      "${homeDirectory}/CLion.app"
+      "/Applications/Calendar.app"
+      "/${pkgs.sublimetext4}/Sublime Text.app"
+      "/Applications/Messages.app"
+      "/Applications/System Settings.app"
+      "/Applications/iPhone Mirroring.app"
       # "/Applications/Zen Browser.app"
       # "/Applications/Vivaldi.app"
       # "/Applications/Ghostty.app"
@@ -28,7 +42,7 @@ in
       # Add your persistent others here
     ];
     show-recents = true;
-    tilesize = 36; # Set the icon size on the dock; default is 64
+    tilesize = 62; # Set the icon size on the dock; default is 64
   };
 
   system.defaults.NSGlobalDomain = {
