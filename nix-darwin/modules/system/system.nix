1bf5236eed31a6dfe2bcd8f8287034a0a5dee564
jblik <88430125+jblik@users.noreply.github.com>
2026-03-13 14:08:07 +0100

    set sublime default editor

--- nix-darwin/modules/system/system.nix
+++ nix-darwin/modules/system/system.nix
@@ -2,13 +2,21 @@
 { config, lib, ... }:
 
 { 
-  system.defaults.screencapture = {
-    location = "~/Documents/Screenshots"; # Set default screenshot location
-    # Add more screencapture settings here
-  };
-  security.pam.services.sudo_local.touchIdAuth = true;
-  
+    system.defaults.screencapture = {
+        location = "~/Documents/Screenshots"; # Set default screenshot location
+        # Add more screencapture settings here
+    };
 
-  
-  
+    security.pam.services.sudo_local.touchIdAuth = true;
+    
+    system.activationScripts.setDefaultApps = {
+        enable = true;
+        text = ''
+            echo "Setting default apps with duti..." >&2
+            # Set Sublime Text as default for unknown/generic file types
+            ${pkgs.duti}/bin/duti -s com.sublimetext.4 public.data all
+            ${pkgs.duti}/bin/duti -s com.sublimetext.4 public.plain-text all
+            ${pkgs.duti}/bin/duti -s com.sublimetext.4 public.content all
+        '';
+    };
 }
