35478921b6670d080a785bf1245214184b6068c1
jblik <88430125+jblik@users.noreply.github.com>
2026-03-13 14:41:27 +0100

    idk can't be found for some reason

--- nix-darwin/modules/apps/services.nix
+++ nix-darwin/modules/apps/services.nix
@@ -5,7 +5,7 @@
   # services.tailscale.overrideLocalDns = true; todo check if this works as expected
   # services.tailscale.package # todo if tailscale from nix doesn't work well then change this to come from mac app store
   
-  services.karabiner-elements.enable = true;
+  # services.karabiner-elements.enable = true; # todo somehow not working on this machine now
 
   # make stats a login item
   launchd.user.agents.stats = {
