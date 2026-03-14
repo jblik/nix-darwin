8313d6a638d081f5562c9deebedf5f6e65ee4e74
jblik <88430125+jblik@users.noreply.github.com>
2026-03-14 19:59:14 +0100

    try this

--- nix-darwin/modules/home-manager/git.nix
+++ nix-darwin/modules/home-manager/git.nix
@@ -14,11 +14,11 @@
           push.autoSetupRemote = true;
 
           aliases = {
-            lg  = log --oneline --graph --decorate;
-            wip = !git add . && git commit -m "wip" && git push;
-            ac  = !git add . && git commit -m;
-            acp = "!f() { git add . && git commit -m \"$1\" && git push; }; f";
-            cb  = "!git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == \"[gone]\" {sub(\"refs/heads/\", \"\", $1); print $1}'); do git branch -D $branch; done";
+            lg  = "log --oneline --graph --decorate";
+            wip = "!git add . && git commit -m \"wip\" && git push";
+            ac  = "!git add . && git commit -m";
+            acp = ''"!f() { git add . && git commit -m \"$1\" && git push; }; f"'';
+            cb  = ''"!git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == \"[gone]\" {sub(\"refs/heads/\", \"\", $1); print $1}'); do git branch -D $branch; done"'';
           };
         };
     
