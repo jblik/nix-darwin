0b445700f6d945020b9acb46fff4956c7ee90c90
jblik <88430125+jblik@users.noreply.github.com>
2026-03-14 11:14:36 +0100

    try again

--- nix-darwin/modules/system/trackpad.nix
+++ nix-darwin/modules/system/trackpad.nix
@@ -13,14 +13,14 @@
 
   system.defaults.NSGlobalDomain = {
   # todo: natural scrolling got enabled on the mouse for some reason
-      "com.apple.swipescrolldirection" = true; # natural scrolling
+      "com.apple.swipescrolldirection" = false; # natural scrolling
       "com.apple.trackpad.scaling" = 3.0; # trackpad tracking speed
   };
 
     # Force macOS to re-read trackpad preferences
     # todo: I don't think these settings work at all! or this part doesn't work 
     # need to try with a restart    
-  system.activationScripts.postActivation.text = lib.mkAfter ''
-    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-  '';
+#  system.activationScripts.postActivation.text = lib.mkAfter ''
+#    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+#  '';
 }
