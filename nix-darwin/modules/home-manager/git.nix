cf394dd75a871a4f6b9094aa3ba5d002afdfdc49
jblik <88430125+jblik@users.noreply.github.com>
2026-03-15 15:36:36 +0100

    git if

--- nix-darwin/modules/home-manager/git.nix
+++ nix-darwin/modules/home-manager/git.nix
@@ -5,34 +5,39 @@
     programs.git = {
         enable = true;
         settings = {
-          user = {
-            name = "jblik";
-            email = "88430125+jblik@users.noreply.github.com";
-          };
-          init.defaultBranch = "master";
-          core.editor = "vim";
-          core.autocrlf = "input";
-          push.autoSetupRemote = true;
+            user = {
+              name = "jblik";
+              email = "88430125+jblik@users.noreply.github.com";
+            };
+            init.defaultBranch = "master";
+            core.editor = "vim";
+            core.autocrlf = "input";
+            push.autoSetupRemote = true;
 
-          alias = {
-            lg  = "log --oneline --graph --decorate";
-            wip = ''!git add . && git commit -m \"wip\" && git push'';
-            ac  = "!git add . && git commit -m";
-            acp = ''!f() { git add . && git commit -m \"$1\" && git push; }; f'';
-            cb  = ''!git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == \"[gone]\" {sub(\"refs/heads/\", \"\", $1); print $1}'); do git branch -D $branch; done'';
-          };
+            alias = {
+                lg  = "log --oneline --graph --decorate";
+                wip = ''!git add . && git commit -m \"wip\" && git push'';
+                ac  = "!git add . && git commit -m";
+                acp = ''!f() { git add . && git commit -m \"$1\" && git push; }; f'';
+                cb  = ''!git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == \"[gone]\" {sub(\"refs/heads/\", \"\", $1); print $1}'); do git branch -D $branch; done'';
+            };
           
-            includeIf."gitdir:~/school/" = {
-              path = "~/.config/git/school.gitconfig";
-            };
-            includeIf."gitdir:~/work/" = {
-              path = "~/.config/git/work.gitconfig";
-            };
+            includes = [
+                {
+                  condition = "gitdir:~/school_local";
+                  # could do something like this: condition = "hasconfig:remote.*.url:git@school.git.com:*/**";
+                  path = "~/school_local/.gitconfig"; # todo make gitconfigs with home manager
+                }
+              {
+                condition = "gitdir:~/Projects/";
+                path = "~/Projects/.gitconfig";
+              }
+            ];
         };
     
         ignores = [
-          ".DS_Store"
-          ".idea/"
+            ".DS_Store"
+            ".idea/"
         ];
     };
 }
