915eb480328d8594dbe98181e25db3569adfedc9
jblik <88430125+jblik@users.noreply.github.com>
2026-03-16 16:22:09 +0100

    make them zsh commands

--- nix-darwin/modules/system/shell.nix
+++ nix-darwin/modules/system/shell.nix
@@ -1,6 +1,7 @@
 { config, lib, username, ... }:
 
 let
+  repoPath = "/Users/${username}/configuration";
   flakePath = "/Users/${username}/configuration/nix-darwin";
   flakeRef = "${flakePath}#${username}";
   flakeUpdateRef = "${flakePath}#${username}-updatehomebrew";
@@ -15,19 +16,37 @@ in
   };
 
   environment.shellAliases = {
-    # todo if there were changes to the flake.lock, commit then
-    nix-update = ''nix flake update --flake ${flakePath} && sudo darwin-rebuild switch --flake ${flakeUpdateRef}'';
-    nix-update-gc = ''nix flake update --flake ${flakePath} && sudo darwin-rebuild switch --flake ${flakeUpdateRef} && nix-gc'';
     nix-rebuild = "sudo darwin-rebuild switch --flake ${flakeRef}";
   };
   
   programs.zsh.interactiveShellInit = ''
+    nix-update() {
+        nix flake update --flake ${flakePath} || return 1
+        
+        if ! git diff -C ${repoPath} --quiet -- ${flakePath}/flake.lock; then
+          git add ${flakePath}flake.lock || return 1
+          git commit -C ${repoPath} -m "update flake.lock" || return 1
+        fi
+        
+        sudo darwin-rebuild switch --flake ${flakeUpdateRef}
+    }
+    nix-update-gc() {
+        nix flake update --flake ${flakePath} || return 1
+        
+        if ! git diff -C ${repoPath} --quiet -- ${flakePath}/flake.lock; then
+          git add ${flakePath}flake.lock || return 1
+          git commit -C ${repoPath} -m "update flake.lock" || return 1
+        fi
+        
+        sudo darwin-rebuild switch --flake ${flakeUpdateRef} || return 1
+        nix-gc
+    }
     nix-gc() {
-      local days="''${1:-10}"
-      echo "Removing generations older than ''${days}d..."
-      nix-collect-garbage --delete-older-than "''${days}d"
-      echo "Optimizing Nix store..."
-      nix-store --optimise
+        local days="''${1:-10}"
+        echo "Removing generations older than ''${days}d..."
+        nix-collect-garbage --delete-older-than "''${days}d"
+        echo "Optimizing Nix store..."
+        nix-store --optimise
     }
 '';
 }
