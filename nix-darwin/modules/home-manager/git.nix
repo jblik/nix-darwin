7d249abdc622bd8f7201bea4c94c34adba92f771
jblik <88430125+jblik@users.noreply.github.com>
2026-03-16 09:35:37 +0100

    try to fix git aliases

--- nix-darwin/modules/home-manager/git.nix
+++ nix-darwin/modules/home-manager/git.nix
@@ -16,10 +16,10 @@
 
             alias = {
                 lg  = "log --oneline --graph --decorate";
-                wip = ''!git add . && git commit -m \"wip\" && git push'';
+                wip = ''!git add . && git commit -m "wip" && git push'';
                 ac  = "!git add . && git commit -m";
-                acp = ''!f() { git add . && git commit -m \"$1\" && git push; }; f'';
-                cb  = ''!git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == \"[gone]\" {sub(\"refs/heads/\", \"\", $1); print $1}'); do git branch -D $branch; done'';
+                acp = ''!f() { git add . && git commit -m "$1" && git push; }; f'';
+                cb  = ''!git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done'';
             };
         };
 
