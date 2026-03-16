e2fce7ad1cd36accee33259cf18bd8c134906bc5
jblik <88430125+jblik@users.noreply.github.com>
2026-03-16 10:11:38 +0100

    don't use the deprecated option

--- nix-darwin/modules/home-manager/zsh.nix
+++ nix-darwin/modules/home-manager/zsh.nix
@@ -18,7 +18,7 @@
           }
         ];
     
-        initExtraFirst = ''
+        initContent = lib.mkBefore ''
           # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
           # Initialization code that may require console input (password prompts, [y/n]
           # confirmations, etc.) must go above this block; everything else may go below.
