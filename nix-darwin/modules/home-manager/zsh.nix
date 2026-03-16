78b7cadaa75fe51185e61ed6fd682412c4e21fbb
jblik <88430125+jblik@users.noreply.github.com>
2026-03-16 10:18:38 +0100

    can only use one initcontent

--- nix-darwin/modules/home-manager/zsh.nix
+++ nix-darwin/modules/home-manager/zsh.nix
@@ -25,10 +25,8 @@
           if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
             source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
           fi
-        '';
-        
-        initContent = lib.mkAfter ''
-        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+          
+          [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
         '';
     };
 }
