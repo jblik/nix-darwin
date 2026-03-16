f70eac680df98d4bb9cafce4efc2f359d2a8f374
jblik <88430125+jblik@users.noreply.github.com>
2026-03-16 16:26:20 +0100

    make it work in every directory

--- nix-darwin/modules/system/shell.nix
+++ nix-darwin/modules/system/shell.nix
@@ -23,9 +23,9 @@ in
     nix-update() {
         nix flake update --flake ${flakePath} || return 1
         
-        if ! git diff -C ${repoPath} --quiet -- ${flakePath}/flake.lock; then
-          git add ${flakePath}flake.lock || return 1
-          git commit -C ${repoPath} -m "update flake.lock" || return 1
+        if ! git -C ${repoPath} diff --quiet -- ${flakePath}/flake.lock; then
+          git add ${flakePath}/flake.lock || return 1
+          git -C ${repoPath} commit -m "update flake.lock" || return 1
         fi
         
         sudo darwin-rebuild switch --flake ${flakeUpdateRef}
@@ -33,9 +33,9 @@ in
     nix-update-gc() {
         nix flake update --flake ${flakePath} || return 1
         
-        if ! git diff -C ${repoPath} --quiet -- ${flakePath}/flake.lock; then
-          git add ${flakePath}flake.lock || return 1
-          git commit -C ${repoPath} -m "update flake.lock" || return 1
+        if ! git -C ${repoPath} diff --quiet -- ${flakePath}/flake.lock; then
+          git add ${flakePath}/flake.lock || return 1
+          git -C ${repoPath} commit -m "update flake.lock" || return 1
         fi
         
         sudo darwin-rebuild switch --flake ${flakeUpdateRef} || return 1
