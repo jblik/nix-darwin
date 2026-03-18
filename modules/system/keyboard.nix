b6a0d1e52716cb6be5451cb4edde852d452c96b8
jblik <88430125+jblik@users.noreply.github.com>
2026-03-18 11:39:19 +0100

    move the location of this

--- modules/system/keyboard.nix
+++ modules/system/keyboard.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ username, ... }:
 
 { 
     system.keyboard = {
@@ -20,15 +20,24 @@
      system.activationScripts.postActivation.text = ''
          echo "resetting manually set keyboard shortcuts"
          defaults delete NSGlobalDomain NSUserKeyEquivalents 2>/dev/null || true
+         
+         # see the below issues as to why these aren't declared in the CustomUserPreferences block:
+         defaults write NSGlobalDomain NSUserKeyEquivalents -dict-add "Move to SAMSUNG" '@~^\\U2192'
+         defaults write NSGlobalDomain NSUserKeyEquivalents -dict-add "Move to Built-in Retina Display" '@~^\\U2192'
+         defaults write NSGlobalDomain NSUserKeyEquivalents -dict-add "Move to E241N" '@~^\\U2190'
+         defaults write NSGlobalDomain NSUserKeyEquivalents -dict-add "Move to S34C65xV" '@~^\\U2190'
+         
+         # Following line should allow us to avoid a logout/login cycle when changing settings
+         sudo -u ${username} /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+
      '';
 
     system.defaults.CustomUserPreferences = {
         NSGlobalDomain = {
+            # if a backslash is needed, we cannot place them here:
+            # https://github.com/NixOS/nix/issues/10082
+            # https://github.com/nix-darwin/nix-darwin/issues/518
             NSUserKeyEquivalents = {
-               "Move to SAMSUNG" = "@~^\\\\U2192"; # Cmd+Alt+Ctrl+Right Arrow
-               "Move to Built-in Retina Display" = "@~^\\\\U2192"; # Cmd+Alt+Ctrl+Right Arrow (left arrow seems to be '\') todo these are being set to '@~^\\\\\\\\U2192'
-               "Move to E241N" = "@~^\\\\U2190"; # Cmd+Alt+Ctrl+Left Arrow
-               "Move to S34C65xV" = "@~^\\\\U21920"; # Cmd+Alt+Ctrl+Left Arrow
                "Fill" = ''@^f''; #Cmd+Ctrl+F
                "Full Screen" = ''@^$f''; #Cmd+Ctrl+Shift+F
            };
