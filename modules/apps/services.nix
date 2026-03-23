e534dc7e8b7eaa7f0879f6501791179c36cd0b67
jblik <88430125+jblik@users.noreply.github.com>
2026-03-23 15:31:09 +0100

    try adding thw two things I casre for

--- modules/apps/services.nix
+++ modules/apps/services.nix
@@ -4,4 +4,28 @@
   services.tailscale.enable = true;
   # services.tailscale.overrideLocalDns = true; # todo check if this works as expected
   # services.tailscale.package # todo if tailscale from nix doesn't work well then change this to come from mac app store
+  
+    launchd.user.agents = {
+      # login items
+      karabiner = {
+        serviceConfig = {
+          RunAtLoad = true;
+          KeepAlive = false;
+          ProgramArguments = [
+            "/usr/bin/open"
+            "/Applications/Nix Apps/Karabiner-EventViewer.app"
+          ];
+        };
+      };
+      alttab = {
+        serviceConfig = {
+          RunAtLoad = true;
+          KeepAlive = false;
+          ProgramArguments = [
+            "/usr/bin/open"
+            "/Applications/Nix Apps/AltTab.app"
+          ];
+        };
+      };
+    };
 }
