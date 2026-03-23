50fa7a62f1ae1120e52501d688af2dc97ef3fe7e
jblik <88430125+jblik@users.noreply.github.com>
2026-03-23 14:35:12 +0100

    try karabiner

--- modules/apps/services.nix
+++ modules/apps/services.nix
@@ -5,7 +5,7 @@
   # services.tailscale.overrideLocalDns = true; # todo check if this works as expected
   # services.tailscale.package # todo if tailscale from nix doesn't work well then change this to come from mac app store
 
-  # services.karabiner-elements.enable = true; # todo somehow not working on this machine now
+  services.karabiner-elements.enable = true; # todo somehow not working on this machine now
 
   launchd.user.agents = {
     # login items
