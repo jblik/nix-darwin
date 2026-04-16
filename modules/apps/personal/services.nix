a753c73608a8f7f6f31e0c8b9942f7e8879fdcbf
jblik <88430125+jblik@users.noreply.github.com>
2026-04-16 15:45:57 +0200

    comment out dns

--- modules/apps/personal/services.nix
+++ modules/apps/personal/services.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   services.tailscale.enable = true;
-  services.tailscale.overrideLocalDns = true; # todo check if this works as expected
+#  services.tailscale.overrideLocalDns = true; # todo check if this works as expected -> don't think so
   # services.tailscale.package # todo if tailscale from nix doesn't work well then change this to come from mac app store
 
   #  launchd.user.agents = {
