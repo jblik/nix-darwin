5c729cdeeebcde21ec2d29e7ec3300d724a324a6
jblik <88430125+jblik@users.noreply.github.com>
2026-04-24 11:15:15 +0200

    sublime default text editor

--- modules/system/finder.nix
+++ modules/system/finder.nix
@@ -1,28 +1,24 @@
 { ... }:
 
 {
-  system.defaults = {
-    finder = {
-      FXPreferredViewStyle = "Nlsv"; # list view default
-      NewWindowTarget = "Home";
-      ShowPathbar = true; # shows the path bar on the bottom of finder
-      QuitMenuItem = true; # allows quitting finder
-      AppleShowAllFiles = true;
-      AppleShowAllExtensions = true;
-      ShowMountedServersOnDesktop = true;
-      ShowRemovableMediaOnDesktop = true;
-    };
-    CustomUserPreferences = {
-      "com.apple.desktopservices" = {
-        DSDontWriteNetworkStores = true;
-        DSDontWriteUSBStores = true;
-      };
+  system.defaults.finder = {
+    FXPreferredViewStyle = "Nlsv"; # list view default
+    NewWindowTarget = "Home";
+    ShowPathbar = true; # shows the path bar on the bottom of finder
+    QuitMenuItem = true; # allows quitting finder
+    AppleShowAllFiles = true;
+    AppleShowAllExtensions = true;
+    ShowMountedServersOnDesktop = true;
+    ShowRemovableMediaOnDesktop = true;
+  };
+  system.defaults.CustomUserPreferences = {
+    "com.apple.desktopservices" = {
+      DSDontWriteNetworkStores = true;
+      DSDontWriteUSBStores = true;
     };
   };
-
-  # todo is this needed
-  #    system.defaults.NSGlobalDomain = {
-  #        AppleShowAllExtensions = true;
-  #        AppleShowAllFiles = true;
-  #    };
+  system.defaults.NSGlobalDomain = {
+    AppleShowAllExtensions = true;
+    AppleShowAllFiles = true;
+  };
 }
--- modules/system/system.nix
+++ modules/system/system.nix
@@ -1,19 +1,15 @@
 { pkgs, homeDirectory, ... }:
-
 {
   system = {
     defaults.screencapture.location = "${homeDirectory}/Documents/Screenshots";
     #    todo find out why this doesn't work here
-    #    activationScripts.setDefaultApps = {
-    #      enable = true;
-    #      text = ''
-    #        echo "Setting default apps with duti..." >&2
-    #        # Set Sublime Text as default for unknown/generic file types
-    #        ${pkgs.duti}/bin/duti -s com.sublimetext.4 public.data all
-    #        ${pkgs.duti}/bin/duti -s com.sublimetext.4 public.plain-text all
-    #        ${pkgs.duti}/bin/duti -s com.sublimetext.4 public.content all
-    #      '';
-    #    };
+    activationScripts.postActivation.text = ''
+      echo "Setting default apps with duti..." >&2
+      # Set Sublime Text as default for unknown/generic file types
+      ${pkgs.duti}/bin/duti -s com.sublimetext.4 public.plain-text all
+      ${pkgs.duti}/bin/duti -s com.sublimetext.4 .txt all
+      ${pkgs.duti}/bin/duti -s com.sublimetext.4 .md all
+    '';
   };
   security.pam.services.sudo_local.touchIdAuth = true;
   # todo: disable airplay receiver (runs on port 5000 always)
