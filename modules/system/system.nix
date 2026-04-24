a39b0bb833d4dfb316e9755c7b58cba39cb23d9c
jblik <88430125+jblik@users.noreply.github.com>
2026-04-24 11:19:08 +0200

    disable airplay receiver

--- modules/system/system.nix
+++ modules/system/system.nix
@@ -1,16 +1,16 @@
-{ pkgs, homeDirectory, ... }:
+{ pkgs, homeDirectory, username, ... }:
 {
   system = {
     defaults.screencapture.location = "${homeDirectory}/Documents/Screenshots";
-    #    todo find out why this doesn't work here
     activationScripts.postActivation.text = ''
-      echo "Setting default apps with duti..." >&2
-      # Set Sublime Text as default for unknown/generic file types
+      echo "setting default apps with duti..."
       ${pkgs.duti}/bin/duti -s com.sublimetext.4 public.plain-text all
       ${pkgs.duti}/bin/duti -s com.sublimetext.4 .txt all
       ${pkgs.duti}/bin/duti -s com.sublimetext.4 .md all
+
+      echo "disabling airplay receiver..."
+      sudo -u ${username} /usr/bin/defaults -currentHost write com.apple.controlcenter.plist AirplayRecieverEnabled -bool false
     '';
   };
   security.pam.services.sudo_local.touchIdAuth = true;
-  # todo: disable airplay receiver (runs on port 5000 always)
 }
