7f6788292be31b9b4aa2b14605ff28ae49434886
jblik <88430125+jblik@users.noreply.github.com>
2026-04-29 10:45:57 +0200

    todo

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -29,7 +29,8 @@
       # maybe think about just configuring it when you set up a new machine
       [[ ! -f ~/.config/p10k/.p10k.zsh ]] || source ~/.config/p10k/.p10k.zsh
       export PATH=$PATH:/opt/homebrew/bin
-#      export DOTNET_ROOT=$HOME/.dotnet
+      # todo: remove this dotnet
+      export DOTNET_ROOT=$HOME/.dotnet
       export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
     '';
   };
