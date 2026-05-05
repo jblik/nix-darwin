60492a56b4dd92666d925263f97465c51b295ccc
jblik <jblik@noreply.codeberg.org>
2026-05-05 14:02:05 +0200

    homemanager default shorter

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,28 +1,20 @@
 {
+  lib,
   user,
   ...
 }:
-let
-  mkUser =
-    {
-      pkgs,
-      lib,
-      ...
-    }:
-    {
-      programs.home-manager.enable = true;
-      home.stateVersion = "25.11";
-      home.username = user.username;
-      home.homeDirectory = lib.mkForce user.homeDirectory;
-      imports = [
-        ./git.nix
-        ./karabiner.nix
-        ./p10k.nix
-        ./sublime.nix
-        ./zsh.nix
-      ];
-    };
-in
 {
-  ${user.username} = mkUser;
+  ${user.username} = {
+    programs.home-manager.enable = true;
+    home.stateVersion = "25.11";
+    home.username = user.username;
+    home.homeDirectory = lib.mkForce user.homeDirectory;
+    imports = [
+      ./git.nix
+      ./karabiner.nix
+      ./p10k.nix
+      ./sublime.nix
+      ./zsh.nix
+    ];
+  };
 }
