a86ae4e938442f08761d24d221f191166e95fb6c
jblik <jblik@noreply.codeberg.org>
2026-05-06 14:01:31 +0200

    re-enable oh my zsh

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -13,10 +13,10 @@ in
   programs.zsh = {
     enable = true;
     autosuggestion.enable = true;
-    #    oh-my-zsh = {
-    #      enable = true;
-    #      plugins = [ "git" ];
-    #    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+    };
 
     plugins = [
       {
