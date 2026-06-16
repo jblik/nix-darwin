0024a1b30ffa0623a6686ded8404bb127f251b0e
jblik <jblik@noreply.codeberg.org>
2026-06-16 12:44:47 +0200

    sign git commits

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -15,9 +15,10 @@
 
       alias = {
         lg = "log --oneline --graph --decorate";
-        wip = ''!git add . && git commit -m "wip" && git push'';
-        ac = "!git add . && git commit -m";
-        acp = ''!f() { git add . && git commit -m "$1" && git push; }; f'';
+        wip = ''!git add . && git commit -S -m "wip" && git push'';
+        c = "git commit -S -m";
+        ac = "!git add . && git commit -S -m";
+        acp = ''!f() { git add . && git commit -S -m "$1" && git push; }; f'';
         cb = ''!git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done'';
       };
     };
