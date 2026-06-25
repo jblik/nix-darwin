b4ccbbd1caba6807e01e6f0e7240e43aaf93fa26
jblik <jblik@noreply.codeberg.org>
2026-06-25 22:36:17 +0200

    more default apps

--- modules/system/system.nix
+++ modules/system/system.nix
@@ -9,10 +9,12 @@
     defaults.screencapture.location = "${user.homeDirectory}/Documents/Screenshots";
     activationScripts.postActivation.text = ''
       echo "setting default apps with duti..."
-      ${lib.getExe pkgs.duti} -s com.sublimetext.4 public.plain-text all
-      ${lib.getExe pkgs.duti} -s com.sublimetext.4 .txt all
-      ${lib.getExe pkgs.duti} -s com.sublimetext.4 .md all
-      ${lib.getExe pkgs.duti} -s com.sublimetext.4 .json all
+
+      for ext in .txt .md .json .yaml .yml .toml .ini .cfg .conf .log .csv \
+                 .js .ts .jsx .tsx .fs .fsx .sh .zsh .bash .rb .go .rs .c .h .cpp \
+                 .html .css .scss .xml .sql .env .gitignore dockerfile public.plain-text; do
+        ${lib.getExe pkgs.duti} -s com.sublimetext.4 $ext all
+      done
 
       # echo "disabling airplay receiver..."
       # sudo -u ${user.username} /usr/bin/defaults -currentHost write com.apple.controlcenter.plist AirplayRecieverEnabled -bool false
