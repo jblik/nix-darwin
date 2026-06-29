0199844df2841d3db607feeb6d684d4362b911b4
jblik <jblik@noreply.codeberg.org>
2026-06-29 21:42:52 +0200

    add ssh keys for work

--- users.nix
+++ users.nix
@@ -31,7 +31,22 @@
       email = "jacob.steenblik@yarowa.com";
     };
     ssh = {
-      # todo
+      "github.com" = {
+        User = "git";
+        IdentityFile = "/Users/wookie/.ssh/id_ed25519";
+      };
+      "vs-ssh.visualstudio.com:v3" = {
+        User = "jacob.steenblik@yarowa.com";
+        IdentityFile = "/Users/wookie/.ssh/azure_devops";
+      };
+      "Host git.internal.master.yoda.cloud" = {
+        User = "git";
+        IdentityFile = "/Users/wookie/.ssh/yarowa_forgejo";
+      };
+      "codeberg.org" = {
+        User = "git";
+        IdentityFile = "/Users/jblik/.ssh/jblik_forgejo";
+      };
     };
   };
 }
