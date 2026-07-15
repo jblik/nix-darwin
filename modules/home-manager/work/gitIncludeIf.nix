84591b8d59331ac62565c6572300301d331b8916
jblik <jblik@noreply.codeberg.org>
2026-07-15 14:11:51 +0200

    malformed

--- modules/home-manager/work/gitIncludeIf.nix
+++ modules/home-manager/work/gitIncludeIf.nix
@@ -4,6 +4,6 @@ user: [
       commit.gpgsign = true;
       user.signingKey = "${user.ssh."git.internal.master.yoda.cloud".IdentityFile}.pub";
     };
-    condition = "hasconfig:remote.*.url:ssh://git@git.internal.master.yoda.cloud/**";
+    condition = "hasconfig:remote.*.url:ssh://git@git.internal.master.yoda.cloud:**";
   }
 ]
