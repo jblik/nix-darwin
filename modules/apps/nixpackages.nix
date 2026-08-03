57bd5607758f887af23ca7582a8405e78d2d0639
jblik <jblik@noreply.codeberg.org>
2026-08-03 10:18:16 +0200

    add bruno

--- modules/apps/nixpackages.nix
+++ modules/apps/nixpackages.nix
@@ -27,6 +27,7 @@ in
       alt-tab-macos
       azure-cli
       ansible # configuration management tool
+      bruno # os postman
       coreutils # gnu core utils
       google-chrome
       gnupg # gpg todo: configure this and pinentry also with home-manager
