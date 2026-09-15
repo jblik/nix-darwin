bdd2bc0a90462f7a0b0e1c03ecd0d9a78fc6c91e
jblik <jblik@noreply.codeberg.org>
2026-09-15 14:42:00 +0200

    jj settings personal

--- modules/home-manager/personal/jjSettings.nix
+++ modules/home-manager/personal/jjSettings.nix
@@ -1,1 +1,28 @@
-_user: { }
+user: {
+  "--scope" = [
+    {
+      "--when".repositories = [ "~/nix-darwin" ];
+      user = {
+        name = "jblik";
+        email = "jblik@noreply.codeberg.org";
+      };
+      signing = {
+        behavior = "own";
+        backend = "ssh";
+        key = "${user.ssh."codeberg.org".IdentityFile}.pub";
+      };
+    }
+    {
+      "--when".repositories = [ "~/school" ];
+      user = {
+        name = "Jacob Steenblik";
+        email = "jacob.steenblik@ost.ch";
+      };
+      signing = {
+        behavior = "own";
+        backend = "ssh";
+        key = "${user.ssh."gitlab.ost.ch".IdentityFile}.pub";
+      };
+    }
+  ];
+}
