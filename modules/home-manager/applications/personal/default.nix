6960d1ef1c282688489afbf8b235d638b8417166
jblik <jblik@noreply.codeberg.org>
2026-09-17 17:10:57 +0200

    feat: add java to jblik

--- modules/home-manager/applications/personal/default.nix
+++ modules/home-manager/applications/personal/default.nix
@@ -1,6 +1,18 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    prismlauncher
+  config,
+  pkgs,
+  ...
+}:
+{
+  home.packages = [
+    pkgs.prismlauncher
   ];
+
+  programs.java = {
+    enable = true;
+    package = pkgs.jdk25;
+  };
+
+  home.file."Library/Java/JavaVirtualMachines/zulu-25.jdk".source =
+    "${config.programs.java.package}/Library/Java/JavaVirtualMachines/zulu-25.jdk";
 }
