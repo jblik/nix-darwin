2ffa8b7d490e71e478b621fcfbfe658f7e1ccf2d
jblik <jblik@noreply.codeberg.org>
2026-09-22 14:31:41 +0200

    feat: manage shared jjui configuration

--- modules/home-manager/jj.nix
+++ modules/home-manager/jj.nix
@@ -17,4 +17,49 @@
     }
     // import ./${user.profile}/jj-settings.nix user;
   };
+
+  programs.jjui = {
+    enable = true;
+    package = null; # don't install user scoped package as it is installed system wide
+    settings = {
+      revisions = {
+        log_batching = true;
+        log_batch_size = 50;
+        template = ''
+          if(self.root(),
+            format_root_commit(self),
+            label(
+              separate(" ",
+                if(self.current_working_copy(), "working_copy"),
+                if(self.immutable(), "immutable", "mutable"),
+                if(self.conflict(), "conflicted"),
+              ),
+              concat(
+                separate(" ",
+                  format_short_change_id_with_change_offset(self),
+                  self.bookmarks(),
+                  format_short_signature(self.author()),
+                  format_timestamp(commit_timestamp(self)),
+                  self.tags(),
+                  self.working_copies(),
+                  format_short_commit_id(self.commit_id()),
+                  format_commit_labels(self),
+                  if(config("ui.show-cryptographic-signatures").as_boolean(),
+                    format_short_cryptographic_signature(self.signature())
+                  ),
+                ) ++ "\n",
+                separate(" ",
+                  if(self.empty(), empty_commit_marker),
+                  if(self.description(),
+                    self.description().first_line(),
+                    label(if(self.empty(), "empty"), description_placeholder),
+                  ),
+                ) ++ "\n",
+              ),
+            ),
+          )
+        '';
+      };
+    };
+  };
 }
