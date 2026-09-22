7d82db30345dbf1ef1b4f972b19e4ce42905d35b
jblik <jblik@noreply.codeberg.org>
2026-09-22 09:29:38 +0200

    alt+' to open notification center

--- modules/system/keyboard.nix
+++ modules/system/keyboard.nix
@@ -19,17 +19,21 @@
       CustomUserPreferences = {
         pbs.NSServicesStatus = {
           # this maps to Services>Files and Folders
-          "com.mitchellh.ghostty - New Ghostty Window Here - openWindow" = {
-            key_equivalent = "^$t";
-          };
+          "com.mitchellh.ghostty - New Ghostty Window Here - openWindow".key_equivalent = "^$t"; # Ctrl+Shift+t
         };
         "com.apple.Safari" = {
           IncludeDevelopMenu = true; # doesn't technically belong here but eh
           NSUserKeyEquivalents = {
-            "Move Tab to New Window" = "~@m";
-            "Move Tab to New Private Window" = "~@m";
-            "Merge All Windows" = "~@$m";
+            "Move Tab to New Window" = "~@m"; # Alt+Cmd+M
+            "Move Tab to New Private Window" = "~@m"; # Alt+Cmd+M
+            "Merge All Windows" = "~@$m"; # Alt+Cmd+Shift+M
           };
+        };
+        NSGlobalDomain.NSUserKeyEquivalents = {
+          # if a backslash is needed (as escape char like f12), we cannot place them here:
+          # https://github.com/NixOS/nix/issues/10082
+          # https://github.com/nix-darwin/nix-darwin/issues/518
+          "Show Notification Center" = "~'"; # Alt+'
         };
       };
     };
