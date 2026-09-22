4cc51ed282a1d6b811173a149395e681c3cf60a7
jblik <jblik@noreply.codeberg.org>
2026-09-22 09:38:24 +0200

    reformat finder

--- modules/system/finder.nix
+++ modules/system/finder.nix
@@ -1,22 +1,22 @@
 {
-  system.defaults.finder = {
-    FXPreferredViewStyle = "Nlsv"; # list view default
-    NewWindowTarget = "Home";
-    ShowPathbar = true; # shows the path bar on the bottom of finder
-    QuitMenuItem = true; # allows quitting finder
-    AppleShowAllFiles = true;
-    AppleShowAllExtensions = true;
-    ShowMountedServersOnDesktop = true;
-    ShowRemovableMediaOnDesktop = true;
-  };
-  system.defaults.CustomUserPreferences = {
-    "com.apple.desktopservices" = {
+  system.defaults = {
+    finder = {
+      FXPreferredViewStyle = "Nlsv"; # list view default
+      NewWindowTarget = "Home";
+      ShowPathbar = true; # shows the path bar on the bottom of finder
+      QuitMenuItem = true; # allows quitting finder
+      AppleShowAllFiles = true;
+      AppleShowAllExtensions = true;
+      ShowMountedServersOnDesktop = true;
+      ShowRemovableMediaOnDesktop = true;
+    };
+    NSGlobalDomain = {
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+    };
+    CustomUserPreferences."com.apple.desktopservices" = {
       DSDontWriteNetworkStores = true;
       DSDontWriteUSBStores = true;
     };
-  };
-  system.defaults.NSGlobalDomain = {
-    AppleShowAllExtensions = true;
-    AppleShowAllFiles = true;
   };
 }
