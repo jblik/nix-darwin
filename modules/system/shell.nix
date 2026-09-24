50a81c679c2e8f46334c34ea761e412eff3b4e1f
jblik <jblik@noreply.codeberg.org>
2026-09-24 09:50:29 +0200

    perf: skip nix-darwin compinit and prompt init that oh-my-zsh and p10k already handle

--- modules/system/shell.nix
+++ modules/system/shell.nix
@@ -13,4 +13,10 @@
       OLLAMA_NO_CLOUD = "true";
     };
   };
+
+  # oh-my-zsh and powerlevel10k from home-manager already run compinit and set the prompt
+  programs.zsh = {
+    enableGlobalCompInit = false;
+    promptInit = "";
+  };
 }
